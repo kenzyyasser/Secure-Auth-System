ae043f65d73046a926ccc03d8cd4bfbd5ff7421f
kenzyyasser <k0nzy.yasser5@gmail.com>
2026-04-30 02:58:13 +0300

    Change unauthorized status code from 401 to 801

--- backend/middleware/role.js
+++ backend/middleware/role.js
@@ -1,6 +1,6 @@
 const authorizeRoles = (...allowedRoles) => {
   return (req, res, next) => {
-    if (!req.user) return res.status(401).json({ message: 'Unauthorized' });
+    if (!req.user) return res.status(801).json({ message: 'Unauthorized' });
     if (!allowedRoles.includes(req.user.role))
       return res.status(403).json({ message: 'Access forbidden: insufficient permissions' });
     next();
